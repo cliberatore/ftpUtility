edc27a3be7673971b9b38306aec7a0ab943a1b4c
Chris Liberatore <chris.liberatore@gmail.com>
2012-06-13 21:12:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FtpUtility::Application.config.secret_token = 'de0b0ff76eb2ec0f0ce7276548ffe5fefa1b63bf8f21decefe07ace50e5ceb2a2c197c7588d9e32dd65e3ec222564b79d7f276c7a0f59c5210a0df98e1a96e2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FtpUtility::Application.config.session_store :cookie_store, key: '_ftpUtility_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FtpUtility::Application.config.session_store :active_record_store
